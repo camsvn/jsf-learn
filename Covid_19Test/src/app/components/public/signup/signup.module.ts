1c9ef29e435a72dfb64c7c43b6444a870df575cf
Amel Salvin <amalsalvin.joseph@bizruntime.org>
2020-12-15 19:41:20 +0530

    Stable SignUp Page

--- Covid_19Test/src/app/components/public/signup/signup.module.ts
+++ Covid_19Test/src/app/components/public/signup/signup.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { IonicModule } from '@ionic/angular';
 
 import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
+import { MaterialDesignFrameworkModule } from '@visur/formgenerator-material'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from 'app/globals/shared/material.module'
 
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     SignupPageRoutingModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MaterialDesignFrameworkModule,
+    MaterialModule,
+    MatFormFieldModule,
+    MatSelectModule
+
   ],
   declarations: [SignupPage]
 })
--- Covid_19Test/src/app/components/public/signup/signup.page.ts
+++ Covid_19Test/src/app/components/public/signup/signup.page.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
+import { DBQueries } from 'app/globals/sqlite/query-operations'
+import { Schemas } from 'app/globals/constants/app-constants'
+import { SQLiteService } from 'app/services/sqlite.service'
+
+import { JsonSchemaFormComponent } from '@visur/formgenerator-core';
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage {
+export class SignupPage implements AfterViewInit  {
+  schema: any;
+  layout: any;
+  options: any;
+  enum: any;
+  data: any;
+  roles: any;
+  images: any;
+  userRoles: any;
+  formulas: any;
+  conditionalFields: any;
+  lockPage: any;
+  lookup: any;
+  state: any;
+  nestedSchema: any;
+  jsonFormOptions: any = {
+    addSubmit: true, // Add a submit button if layout does not have one
+    debug: false, // Don't show inline debugging information
+    loadExternalAssets: true, // Load external css and JavaScript for frameworks
+    returnEmptyFields: false, // Don't return values for empty input fields
+    setSchemaDefaults: true, // Always use schema defaults for empty fields
+    defautWidgetOptions: { feedback: true,hideRequired:false }, // Show inline feedback icons,
+    rowHeight: 20,
+    timeFormat: 24,dataTableHeaderHeight: 50,
+    dataTableRowHeight: 50,
+    dataTableMaxHeight:500 
+  };
+
+  STATE = {"userName":"chhoturam.jat@bizruntime.com","fullName":"Amal Salvin","firstName":"chhoturam","lastName":"jat","email":"chhoturam.jat@bizruntime.com","userId":"21059ad1-190f-427e-3045-1bd3411426c5","companyId":null,"companyName":null,"tenantId":"f2dedf6e-393e-42bc-9bb3-e835a1063b30","tenantName":"constructiontest","projectId":null,"projectName":null,"projectUOM":null};
+  
+  @ViewChild(JsonSchemaFormComponent)
+  formGenerator: JsonSchemaFormComponent
+
+  constructor(private _SQLiteService: SQLiteService){}
+
+  ngAfterViewInit(){
+    this.getSchema();
+  }
+
+  async getSchema(){
+    let retRes = await this._SQLiteService.query(DBQueries.getSchema(Schemas.Signup))
+    if(retRes.values) {
+      let signupSchema = JSON.parse(retRes.values[0].schema);
+      this.generateForm(signupSchema);
+    }
+  }
+
+  generateForm(newFormString: string) {
+    try {
+      const value = JSON.parse(newFormString);
+      this.data = value.data
+      this.schema = value?.schema;
+      this.layout = value?.layout;
+      this.roles = value?.roles;
+      this.images=value?.images;
+      this.userRoles = value?.userRoles;
+      this.formulas = value?.formulas;
+      this.conditionalFields = value?.conditionalFields;
+      this.enum = value?.enums
+      this.lockPage = value?.lockPage;
+      this.lookup = value?.lookup
+      this.state = this.STATE
+      this.nestedSchema = value?.nestedSchema;
+    } catch (e) {
+      console.log(e)
+    }    
+  }
+
+  onSubmitFn(data){
+    console.log(data);
+  }
 
 }
